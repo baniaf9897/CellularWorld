b90fa63551eaef99b53faa3b923d6c2efdb445f2
Fabian Töpfer <fabian.toepfer@tu-dresden.de>
2021-06-20 18:10:53 +0200

    creating cell struct

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Cell 
+{
+    public Vector3 m_pos;
+    public Color m_color;
+    
+}
---
+++ Assets/Scripts/SimulationManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimulationManager : MonoBehaviour
+{
+    public int m_NumCells = 1;
+
+    public GameObject WorldPlane = null;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateRandomCellDistribution()
+    {
+        for(int i = 0; i < m_NumCells; i++)
+        {
+
+        };
+    }
+}
